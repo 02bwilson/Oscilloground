70792ffd78b9da44edd8b33a5eeb47e2708a7138
Bryce W <58643544+02bwilson@users.noreply.github.com>
2023-08-13 12:08:57 -0400

    Added more signals, added ability to load from file. Added message box for invalid names. Fixed style of borders.

--- Widgets/AddSignal.py
+++ Widgets/AddSignal.py
@@ -1,6 +1,6 @@
 from PyQt6.QtCore import Qt, pyqtSignal
 from PyQt6.QtGui import QDoubleValidator
-from PyQt6.QtWidgets import QGridLayout, QComboBox, QDialog, QLabel, QLineEdit, QPushButton
+from PyQt6.QtWidgets import QGridLayout, QComboBox, QDialog, QLabel, QLineEdit, QPushButton, QMessageBox
 
 
 class AddSignal(QDialog):
@@ -29,13 +29,13 @@ class AddSignal(QDialog):
         self.signalNameList = list()
 
         self.functionComboBox = QComboBox()
-        self.functionComboBox.addItems(["sin", "cos", "square", "sawtooth", "constant"])
+        self.functionComboBox.addItems(["sin", "cos", "square", "sawtooth", "constant", "random"])
 
         self.operatorComboBox = QComboBox()
         self.operatorComboBox.addItems(["+", "*", "-", "/"])
 
         self.addButton = QPushButton("Add")
-        self.addButton.pressed.connect(self.addSignal)
+        self.addButton.pressed.connect(lambda: self.addSignal(None))
 
         self.exampleLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.exampleLabel.setStyleSheet("text-align: center; font-size:12pt")
@@ -70,8 +70,8 @@ class AddSignal(QDialog):
     def removeSignal(self, signalName):
         if signalName in self.signalNameList:
             self.signalNameList.remove(signalName)
-    def addSignal(self):
 
+    def addSignal(self, payload):
         data = {"name": self.nameLineEdit.text(),
                 "function": self.functionComboBox.currentText(),
                 "operator": self.operatorComboBox.currentText(),
@@ -79,11 +79,48 @@ class AddSignal(QDialog):
                 "beta": self.betaLineEdit.text(),
                 "gamma": self.gammaLineEdit.text()}
 
-        if data["name"] in  self.signalNameList:
-            self.nameLineEdit.setStyleSheet("border: 1px solid red")
+        if payload is not None:
+            data = {"name": payload[0],
+                    "function": payload[5],
+                    "operator": payload[1],
+                    "alpha": payload[2],
+                    "beta": payload[3],
+                    "gamma": payload[4]}
 
+        invalidFlag = False
+        if data["name"] in self.signalNameList or data["name"] in ["", None] or '::' in data['name']:
+            mbox = QMessageBox()
+            mbox.setWindowTitle('Name Error!')
+            mbox.setText('There is a problem with the name of your signal!\n\n'
+                         'The name cannot be a duplicate, and \n'
+                         "it cannot be empty or  contain '::'")
+            mbox.exec()
+            self.nameLineEdit.setStyleSheet("border: 1px solid red")
+            invalidFlag = True
+        else:
+            self.nameLineEdit.setStyleSheet('border: none;')
+
+        checkArray = ["", None, "."]
+        if data["alpha"] in checkArray:
+            self.alphaLineEdit.setStyleSheet('border: 1px solid red')
+            invalidFlag = True
+        else:
+            self.alphaLineEdit.setStyleSheet('border: none;')
+        if data["beta"] in checkArray:
+            self.betaLineEdit.setStyleSheet('border: 1px solid red')
+            invalidFlag = True
+        else:
+            self.betaLineEdit.setStyleSheet('border: none;')
+        if data["gamma"] in checkArray:
+            self.gammaLineEdit.setStyleSheet('border: 1px solid red')
+            invalidFlag = True
+        else:
+            self.gammaLineEdit.setStyleSheet('border: none;')
+        if invalidFlag:
             return
 
+        for lineEdit in [self.nameLineEdit, self.alphaLineEdit, self.betaLineEdit, self.gammaLineEdit]:
+            lineEdit.setStyleSheet('border: none')
 
         self.addSignalSignal.emit(data)
 
